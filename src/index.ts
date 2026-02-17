e11f9d5a657026331aefb34f4bbf44d1be271401
PerryFinn <perryfinn@163.com>
2026-02-17 22:31:21 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import openapi from '@elysiajs/openapi';
 import { Elysia } from 'elysia';
 import { userRoute } from './user-route';
 
+console.log('process.env.PORT :>> ', process.env.PORT);
 const app = new Elysia()
   .use(openapi())
   .use(userRoute)
   .get('/', () => 'Hello Elysia')
 
-  .listen(15087);
+  .listen(process.env.PORT ?? 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
